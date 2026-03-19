94921148cc4c27d03327fe1d3f43f26712371671
hardstone <zhaoxuanmail@163.com>
2026-03-19 22:55:08 +0800

    [feat] project

--- django_project/learning_log/urls.py
+++ django_project/learning_log/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ArticleBlog project.
+URL configuration for learning_log project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r"^admin/", admin.site.urls),
+    url(r'', include('learning_logs.urls', namespace='learning_logs')),
 ]
---
+++ django_project/learning_logs/urls.py
@@ -0,0 +1,9 @@
+"""定义learning_logs的URL模式"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
